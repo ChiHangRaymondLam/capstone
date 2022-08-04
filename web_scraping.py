e3478d05889cfcd34ed6143d07d14dd894ae7f0b
Chi Hang (Raymond) Lam <rchlam27@gmail.com>
2022-08-05 10:19:48 +1200

    first web scraping template

---
+++ web_scraping.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+
+
+def scrapeWikiArticle(url):
+	response = requests.get(url)
+	
+    # Create a BeautifulSoup objet from the HTML
+	soup = BeautifulSoup(response.content, 'html.parser')
+
+	title = soup.find(id="firstHeading")
+	print(title.text)
+
+    # Find all the a tags under id is bodyContent
+	allLinks = soup.find(id="bodyContent").find_all("a")
+	random.shuffle(allLinks)
+	link_to_scrape = 0
+
+	for link in allLinks:
+		# Scrape the wiki articles only
+		if link['href'].find("/wiki/") == -1: 
+			continue
+
+		# Use this link to scrape
+		link_to_scrape = link
+		break
+
+	scrapeWikiArticle("https://en.wikipedia.org" + link_to_scrape['href'])
+
+scrapeWikiArticle("https://en.wikipedia.org/wiki/Supply_chain_management")
